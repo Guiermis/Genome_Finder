6ba2d44409aa84ad977a0e1d85606b72cf50ecde
Guilherme Rodrigues <guilhermerodrigues717@yahoo.com>
2022-07-07 14:51:53 -0300

    Models Update, including a ChartDataModel, for chart on Comunity page

---
+++ Genome_Finder/Models/Base_Nitrogen.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Genome_Finder.Models
+{
+    public class Base_Nitrogen
+    {
+        [Required(ErrorMessage = "Digite a Espécie do Gene")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o Código do NCBI")]
+        public string NCBICod { get; set; }
+
+        [Required(ErrorMessage = "Digite a Base Nitrogenada")]
+        public string Gene { get; set; }
+
+        public int searchnum = 1;
+
+    }
+}
---
+++ Genome_Finder/Models/ChartDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Genome_Finder.Models
+{
+    public class ChartDataModel
+    {
+        public string ChartName { get; set; }
+        public List<string> ChartLabel { get; set; }
+        public List<int> ChartData { get; set; }
+        
+    }
+}
