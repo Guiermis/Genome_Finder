6d2eb770d40ccb7fb88e5c1af3482c99260bf1bf
Guilherme Rodrigues <guilhermerodrigues717@yahoo.com>
2022-07-07 14:50:10 -0300

    Update on Controllers

---
+++ Genome_Finder/Controllers/GenomeController.cs
@@ -0,0 +1,24 @@
+﻿using Genome_Finder.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Genome_Finder.Controllers
+{
+    public class GenomeController : Controller
+    {
+        public IActionResult Compare()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Compare(Base_Nitrogen model)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
--- Genome_Finder/Controllers/HomeController.cs
+++ Genome_Finder/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
 ﻿using Genome_Finder.Models;
+using Genome_Finder.Views;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Genome_Finder.DataLibrary;
+using static Genome_Finder.DataLibrary.BusinessLogic.GenomeProcessor;
+using System.Data;
+using System.Data.Entity;
+using System.Collections.Generic;
 
 namespace Genome_Finder.Controllers
 {
@@ -33,9 +39,76 @@ namespace Genome_Finder.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult CompareRes(Base_Nitrogen model)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                var data = LoadBases();
+
+                List<Base_Nitrogen> bases = new List<Base_Nitrogen>();
+
+                foreach (var row in data)
+                {
+                    bases.Add(new Base_Nitrogen
+                    {
+                        Gene = row.Gene,
+                        Nome = row.Nome,
+                        NCBICod = row.NBCICod,
+                        searchnum = row.searchNum
+                    });
+                }
+
+                if (data.Any(x => x.Nome == model.Nome))
+                {
+                    UpdateGenome(model.Nome);
+                } else
+                {
+                    CreateGenome(model.Gene, model.Nome, model.searchnum, model.NCBICod);
+                }
+            }
+            return NoContent();
+
+
+        }
+
         public IActionResult Comunity()
         {
-            return View();
+            var data = LoadBases();
+            List<int> searchCount = new List<int>();
+            List<string> nomesLista = new List<string>();
+            var nomes = data.Select(x => x.Nome);
+            var searchs = data.Select(x => x.searchNum).Distinct();
+            List<Base_Nitrogen> bases = new List<Base_Nitrogen>();
+
+            foreach (var row in data)
+            {
+                bases.Add(new Base_Nitrogen
+                {
+                    Gene = row.Gene,
+                    Nome = row.Nome,
+                    NCBICod = row.NBCICod,
+                    searchnum = row.searchNum
+                });
+            }
+
+                nomesLista.AddRange(data.Select(x => x.Nome));
+
+                searchCount.AddRange(data.Select(x => x.searchNum));
+
+
+            var chartDataModel = new Models.ChartDataModel()
+            {
+                ChartData = searchCount,
+                ChartLabel = nomesLista,
+                ChartName = "Numero de Pesquisas"
+
+            };
+
+            return View(chartDataModel);
+
         }
 
         public IActionResult Privacy()
